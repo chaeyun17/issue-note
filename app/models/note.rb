5e2d7b7bff211ee44ea2dbc06cc24b3cd55327aa
Chaeyun Lee <hopefulyun@gmail.com>
2021-04-14 22:04:43 +0900

    [기능] note Validation

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,5 @@
 class Note < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
   has_one_attached :photo
   has_rich_text :content
   has_many :comments
